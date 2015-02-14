b333466ece693da8a3d734ed3122e819efcfa81c
jyothikumar <jyothikumar1990@gmail.com>
2015-02-14 16:30:37 +0530

    Intital commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.scroll').on('click', function(e) {
+		e.preventDefault();
+		$('.active').removeClass('active');
+		$(this).addClass('active');
+		var x = $(this).attr('href');
+		$('body').animate({
+			scrollTop: $(''+x+'').offset().top 
+		}, 1000);
+	});
+});
